865acdedf8e14aaebc6b6dfc0d968ffa260d812f
Jeferson Echavarria <jfrson24@gmail.com>
2023-02-26 17:09:09 -0500

    Class HourlyEmployee.cs created

---
+++ MyFirstOOPCode/MyFirstOOPCode/HourlyEmplyee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstOOPCode
+{
+    public class HourlyEmplyee : Employee
+    {
+        #region Properties
+        public double Hours { get; set; }
+        public decimal HoursValue { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Hours * HoursValue;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"Hours.....................{Hours:N2}\n\t" +
+                $"Value per hour ...........{HoursValue:C2}\n\t" +
+                $"Value to Pay..............{GetValueToPay():C2}";
+        }
+        #endregion
+    }
+}
--- MyFirstOOPCode/MyFirstOOPCode/Program.cs
+++ MyFirstOOPCode/MyFirstOOPCode/Program.cs
@@ -45,6 +45,45 @@ namespace MyFirstOOPCode
                 //Console.WriteLine(salaryEmployeeObject.ToString());
                 Console.WriteLine("");
 
+                //Console.WriteLine("What's your first name?");
+                //string firstName = Console.ReadLine();
+
+                //Console.WriteLine($"{firstName} what's your last name?");
+                //string lastName = Console.ReadLine();
+
+                //Console.WriteLine($"{firstName} when is your birthday? yyyy/mm/dd");
+                //string[] birthday = Console.ReadLine().Split('/');
+                //var dateObjectBirthDate = new Date(Convert.ToInt16(birthday[0]), Convert.ToInt16(birthday[1]), Convert.ToInt16(birthday[2]));
+
+                //Console.WriteLine($"{firstName} when they hired you? yyyy/mm/dd");
+                //string[] hireday = Console.ReadLine().Split('/');
+                //var dateObjectHiringDate = new Date(Convert.ToInt16(hireday[0]), Convert.ToInt16(hireday[1]), Convert.ToInt16(hireday[2]));
+
+                //Console.WriteLine($"{firstName} you are employed?");
+                //bool isActive = Convert.ToBoolean(Console.ReadLine());
+
+                //Console.WriteLine($"{firstName} Type ypur in percentage?");
+                //double commissionPercentage = Convert.ToDouble(Console.ReadLine());
+
+                //Console.WriteLine($"{firstName} Type your sales?");
+                //decimal sale = Convert.ToDecimal(Console.ReadLine());
+
+                //Employee commissionEmployee = new CommissionEmployee() 
+                //{
+                //    FirstName = firstName,
+                //    LastName = lastName,
+                //    BirthDate = dateObjectBirthDate,
+                //    HiringDate = dateObjectHiringDate, 
+                //    IsActive = isActive,
+                //    CommissionPercentge = commissionPercentage,
+                //    Sales = sale
+                //};
+                //Console.WriteLine("***********************");
+                //Console.WriteLine("* Commission Employee *");
+                //Console.WriteLine("***********************");
+                //Console.WriteLine("");
+                //Console.WriteLine(commissionEmployee.ToString());
+
                 Console.WriteLine("What's your first name?");
                 string firstName = Console.ReadLine();
 
@@ -62,27 +101,27 @@ namespace MyFirstOOPCode
                 Console.WriteLine($"{firstName} you are employed?");
                 bool isActive = Convert.ToBoolean(Console.ReadLine());
 
-                Console.WriteLine($"{firstName} Type ypur in percentage?");
-                double commissionPercentage = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{firstName} your bumber of hours?");
+                double hours = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine($"{firstName} Type your sales?");
-                decimal sale = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine($"{firstName} your per hour?");
+                decimal hoursValue = Convert.ToDecimal(Console.ReadLine());
 
-                Employee commissionEmployee = new CommissionEmployee() 
+                Employee HourlyEmployee = new HourlyEmplyee()
                 {
                     FirstName = firstName,
                     LastName = lastName,
                     BirthDate = dateObjectBirthDate,
-                    HiringDate = dateObjectHiringDate, 
+                    HiringDate = dateObjectHiringDate,
                     IsActive = isActive,
-                    CommissionPercentge = commissionPercentage,
-                    Sales = sale
+                    Hours = hours,
+                    HoursValue = hoursValue
                 };
-                Console.WriteLine("***********************");
-                Console.WriteLine("* Commission Employee *");
-                Console.WriteLine("***********************");
+                Console.WriteLine("*******************");
+                Console.WriteLine("* Hourly Employee *");
+                Console.WriteLine("********************");
                 Console.WriteLine("");
-                Console.WriteLine(commissionEmployee.ToString());
+                Console.WriteLine(HourlyEmployee.ToString());
             }
             catch (Exception ex)
             {
